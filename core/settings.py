398d1b8aed3131ffffcf4dfe0e0029fffa3ce6c9
Santhosh K <santhoshsiva990@gmail.com>
2025-09-13 18:32:19 +0530

    Changed to mysql for free deployment purpose.

--- core/settings.py
+++ core/settings.py
@@ -37,13 +37,9 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 # PostgreSQL — fill with your credentials
 DATABASES = {
-    'default':{
-        'ENGINE':'django.db.backends.postgresql',
-        'NAME':'expensetracker',
-        'USER':'postgres',
-        'PASSWORD':'postgres',
-        'HOST':'127.0.0.1',
-        'PORT':'5432',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
